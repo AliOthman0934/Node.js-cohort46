45dc03cf4f57d7cfcec0b4aae859eb604756e674
Ali Othman <alioth840@gmail.com>
2024-02-06 14:25:44 +0100

    I have completed the w2 assignments

--- assignments/HackYourTemperature/server.js
+++ assignments/HackYourTemperature/server.js
@@ -1,18 +1,41 @@
 import express from 'express';
-
+import fetch from 'node-fetch';
+import { keys } from './sources/key.js';
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hallo from backend to frontend!")
+app.get('/', (req, res) => {
+    res.send('Hello from backend to frontend!');
 });
 
-app.post("/weather", (req, res) => {
-    const cityName = req.body.cityName;
-    res.send(`The city name is: ${cityName}`)
+app.post('/weather', async (req, res) => {
+    const { cityName } = req.body;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${keys.API_KEY}`;
+    const options = {
+        method: 'GET', 
+        headers: { 'Content-Type': 'application/json' },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        if (data.cod === '404') {
+            res.json({ weatherText: 'City is not found!' });
+        } else {
+            const temperature = data.main.temp;
+            const temperatureCelsius = temperature - 273.15;
+            res.json({ weatherText: `Weather in ${cityName}: ${temperatureCelsius.toFixed(0)}°C` });
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
---
+++ assignments/HackYourTemperature/sources/key.js
@@ -0,0 +1,3 @@
+export const keys = {
+  API_KEY : "95fa0149f9487f69f2f583162428bc3f"
+} 
